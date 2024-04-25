578ad13ec0fff54d4c73671ec19927d6dd3e7e31
Aydan <91035120+Aydanjb@users.noreply.github.com>
2024-04-25 13:51:46 -0400

    Added card display functionality and updated styles

--- brownapp/flashcards/static/flashcards/scripts/script.js
+++ brownapp/flashcards/static/flashcards/scripts/script.js
@@ -1,6 +1,18 @@
 const createDeckBtn = document.getElementById('createDeck');
 const decks = document.getElementById('decks');
+const deck = document.querySelectorAll('.deck');
+const main = document.querySelector('main');
 
+const cards = [
+    {
+    'front': 'What is the captial of France?',
+    'back': 'Paris'
+    },
+    {
+    'front': 'What is the captial of Germany?',
+    'back': 'Berlin'
+    }
+]
 
 function addDeckButton() {
     const deck = document.createElement('button');
@@ -9,5 +21,24 @@ function addDeckButton() {
     createDeckBtn.insertAdjacentElement('beforebegin', deck);
 }
 
+function displayCards() {
+    cards.forEach(element => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <div class="front">
+                <p>${element.front}</p>
+            </div>
+            <div class="back">
+                <p>${element.back}</p>
+            </div>
+        `;
+        main.appendChild(card);
+    });
+}
+
+deck.forEach(element => {
+    element.addEventListener('click', displayCards);
+});
 createDeckBtn.addEventListener('click', addDeckButton);
 
